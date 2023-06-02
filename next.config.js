fe08c04e8c7ce8a1fdffb33567664e1952cec6e3
oxycaster <480125+oxycaster@users.noreply.github.com>
2023-06-03 04:42:04 +0900

    画像などのstatic asset周りの扱いを導入
    …する方法を調べながらやっていたのだが、どうもこれはやはりサーバーサイドでの画像生成とかそう言った点が密結合になっていて、フロントを完全に静的で独立したSPAとして出力するのは現時点では無理そうというのが見えてきてしまったので、この段階で諦め

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withOptimizedImages = require('next-optimized-images');
+
+const nextConfig = withOptimizedImages({
   reactStrictMode: true,
   swcMinify: true,
-}
+  handleImages: ['jpeg', 'png', 'svg'],
+  images: {
+    unoptimized: true
+  }
+});
 
 module.exports = nextConfig
